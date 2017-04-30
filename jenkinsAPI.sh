e083e38a4c0d9bc2e4d4a02f57cd29d3823c11dc
Alex.Baptista <alex.baptista@concrete.com.br>
2017-04-30 16:22:17 -0300

    criando funções para start e get de infos

--- jenkinsAPI.sh
+++ jenkinsAPI.sh
@@ -1,35 +1,79 @@
 #!/bin/bash
+# -----------------------------------------------------------------------------
+#
+# jenkinsAPI.sh - Realiza a inicialização e obtém status remotamente de um job cadastrado no Jenkins
+#
+# -----------------------------------------------------------------------------
+#
+#
+#
+#
 
 # Config de valores para execução do script
-#Jenkins_url=
-#Job_name=
-#Job_token=
-#User_api=
-#User_token=
+# Exemplo:
+# Jenkins_url="meujenkins.com.br:port"
+# Job_name="ave_ridicula"
+# Job_token=""
+# User_api="ave"
+# User_token=""
 
+# startBuild() - Função para realizar o start remoto do job no jenkins
+# Sobre:
+# Inicialmente é validado o status http (200), para então retornar o número gerado na "QUEUE" do jenkins.
+# O retorno será usado na função getBuildNumber(QUEUE) para obtermos o número do JOB inicializado
 
-function startBuild()
-{
-  response=$(curl -i -s -m 10 --netrc -X GET "http://$Jenkins_host/job/$Job_name/build?token=$Job_token" --user "$User_api:$User_token")
+function startBuild() {
+  response=$(curl -i -s -m 5 --netrc -X GET "http://$Jenkins_host/job/$Job_name/build?token=$Job_token" --user "$User_api:$User_token")
   http_code=$(echo "$response" | grep HTTP | awk '{print $2}')
 
   if [[ $http_code == '201' ]]
   then
     number_queue=$(echo "$response" | grep Location | cut -d\/ -f6)
-    status_queue=$(curl -s -X GET "http://$Jenkins_host/queue/item/$number_queue/api/xml" --user "$User_api:$User_token")
-
-    echo $status_queue
-
+    echo $number_queue
+    exit 0
   elif [[ ! -z $http_code && $http_code != '201' ]]
   then
     echo "Error - $http_code - check job data configuration";
     exit 1
   else
-    echo "Error curl - time-out or invalid host - '$Jenkins_host'"
+    echo "Error - time-out or invalid host - '$Jenkins_host'"
     exit 1
   fi
 }
 
+function getBuildNumber() {
+    number_queue=$1
+    status_queue=$(curl -i -s -m 5 --netrc -X GET "http://$Jenkins_host/queue/item/$number_queue/api/xml" --user "$User_api:$User_token")
+    http_code=$(echo "$status_queue" | grep HTTP | awk '{print $2}')
+
+    if [[ $http_code == '200' ]]
+    then
+      echo "$status_queue"
+      exit 0
+    elif [[ ! -z $http_code && $http_code != '201' ]]
+    then
+      echo "Error - $http_code - check queue number configuration";
+      exit 1
+    else
+      echo "Error - time-out or invalid host - '$Jenkins_host'"
+      exit 1
+    fi
+}
+
+function start() {
+  #echo "Requesting ..."
+  #result=$(startBuild)
+
+  if startBuild
+  then
+    echo "success"
+  else
+    exit "fails"
+  fi
+  #echo "Getting job ID ..."
+  #getBuildNumber $result
+}
+
 case $1 in
-  start) startBuild;;
+  start) start;;
 esac
